55929720464a1d39a67e866eee25124681f8c9c4
V!@d <52937367+JudasOTD@users.noreply.github.com>
2023-02-21 14:06:38 +0200

    Update tcp_server.py

--- Very-Simple-TCP/tcp_server.py
+++ Very-Simple-TCP/tcp_server.py
@@ -1,6 +1,8 @@
 import socket as sck
 
 
+# SOCK_STREAM - [TCP] type of transport protocol used for communication
+# AF_INET - IPv4 address family
 SOCKET = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
 # A socket is an object which represents an endpoint of a communications channel 
 HOST = "127.0.0.1" # Loopback interface
@@ -13,6 +15,6 @@ while True:
     c, a = SOCKET.accept()
     print('C:/>  ', a)
     # Program opens a port on localhost and waits for client to connect
-    c.send(b"Thank U 4 connecting!")
+    c.send(b"Thank YOU 4 connecting!")
     c.close()
 
