6b1ee72321e2e6809305111fe66fa5b61c1473a3
V!@d <52937367+JudasOTD@users.noreply.github.com>
2023-03-06 13:34:00 +0200

    Updated tcp_client.py

--- Very-Simple-TCP/tcp_client.py
+++ Very-Simple-TCP/tcp_client.py
@@ -1,11 +1,11 @@
 import socket as sck
 
 
-SOCKET = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
-LOCALHOST = "127.0.0.1"
+LOCALHOST = "127.0.0.1" # LAN IPv4 address
 PORT = 8000
-
 CREDENTIALS = tuple((LOCALHOST, PORT))
+  
+SOCKET = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
 SOCKET.connect(CREDENTIALS)
 print(SOCKET.recv(128).decode('UTF-8'))
 
