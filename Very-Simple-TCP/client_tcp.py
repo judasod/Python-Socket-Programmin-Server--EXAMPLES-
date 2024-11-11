7de2b82f8241e582de8b852fb200bfbdaa7b89ae
LAPSUS$ <52937367+judasod@users.noreply.github.com>
2024-11-11 05:06:20 +0200

    Update and rename tcp_client.py to client_tcp.py

---
+++ Very-Simple-TCP/client_tcp.py
@@ -0,0 +1,12 @@
+import socket as sck
+
+
+_PORT = 8000
+LOCALHOST = "127.0.0.1" # LAN IPv4 Address 
+CREDENTIALS = tuple((LOCALHOST, _PORT))
+ 
+SOCKET = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
+SOCKET.connect(CREDENTIALS)
+
+print(SOCKET.recv(128).decode('UTF-16'))
+
